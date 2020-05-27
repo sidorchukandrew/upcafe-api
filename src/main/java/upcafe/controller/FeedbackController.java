f26c9a7f9e703e655cc1eae22a1f54b3f6a76681
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2020-05-27 12:14:29 -0400

    Added common endpoints for feedback controller

--- src/main/java/upcafe/controller/FeedbackController.java
+++ src/main/java/upcafe/controller/FeedbackController.java
@@ -1,7 +1,48 @@
 package upcafe.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController("/api/v1")
 public class FeedbackController {
+
+    @GetMapping(path = "/features")
+    public void getFeatureRequests() {
+
+    }
+
+    @GetMapping(path = "/features/{id}")
+    public void getFeatureRequestById(@PathVariable(name = "id") int id) {
+
+    }
+
+    @PostMapping(path = "/features")
+    public void saveNewFeatureRequest() {
+
+    }
+
+    @PutMapping(path = "/features/{id}")
+    public void updateFeatureRequestById(@PathVariable(name = "id") int id) {
+
+    }
+
+    @GetMapping(path = "/bugs")
+    public void getBugs() {
+
+    }
+
+    @GetMapping(path = "/bugs/{id}")
+    public void getBugById(@PathVariable(name = "id") int id) {
+
+    }
+
+    @PostMapping(path = "/bugs")
+    public void saveNewBugReport() {
+
+    }
+
+    @PutMapping(path = "/bugs/{id}")
+    public void updateBugById(@PathVariable(name = "id") int id) {
+
+    }
+
 }
