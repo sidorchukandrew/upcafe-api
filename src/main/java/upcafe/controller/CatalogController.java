845a11a8d9cd6f30dede62c84cd66f20c7957801
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2020-06-19 22:11:35 -0400

    Renamed endpoint for creating images

--- src/main/java/upcafe/controller/CatalogController.java
+++ src/main/java/upcafe/controller/CatalogController.java
@@ -1,7 +1,6 @@
 package upcafe.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,39 +19,38 @@ import com.squareup.square.models.CatalogObject;
 
 import upcafe.dto.catalog.CatalogDTO;
 import upcafe.dto.catalog.CatalogInventoryUpdate;
-import upcafe.dto.catalog.CategoryDTO;
 import upcafe.dto.catalog.ImageDTO;
-import upcafe.dto.menu.MenuItemDTO;
 import upcafe.entity.catalog.Image;
 import upcafe.service.CatalogService;
 
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping(path = "/api/v1/catalog")
 public class CatalogController {
 
     @Autowired
     private CatalogService catalogService;
+//
+//    @GetMapping(path = "/catalog")
+//    @PreAuthorize(value = "hasAnyRole('CUSTOMER', 'STAFF', 'ADMIN')")
+//    public Map<String, List<MenuItemDTO>> getCatalogByCategory(@RequestParam(name = "category") String category) {
+//        Map<String, List<MenuItemDTO>> catalogResponse = new HashMap<String, List<MenuItemDTO>>();
+//        catalogResponse.put("items", catalogService.getItemsForCategory(category));
+//
+//        return catalogResponse;
+//    }
 
-    @GetMapping(path = "/catalog")
-    @PreAuthorize(value = "hasAnyRole('CUSTOMER', 'STAFF', 'ADMIN')")
-    public Map<String, List<MenuItemDTO>> getCatalogByCategory(@RequestParam(name = "category") String category) {
-        Map<String, List<MenuItemDTO>> catalogResponse = new HashMap<String, List<MenuItemDTO>>();
-        catalogResponse.put("items", catalogService.getItemsForCategory(category));
+//    @GetMapping(path = "/categories")
+//    @PreAuthorize(value = "hasAnyRole('CUSTOMER', 'STAFF', 'ADMIN')")
+//    public Map<String, List<CategoryDTO>> getCategories() {
+//        Map<String, List<CategoryDTO>> categoriesResponse = new HashMap<String, List<CategoryDTO>>();
+//        categoriesResponse.put("categories", catalogService.getCategories());
+//
+//        return categoriesResponse;
+//
+//    }
 
-        return catalogResponse;
-    }
-
-    @GetMapping(path = "/categories")
-    @PreAuthorize(value = "hasAnyRole('CUSTOMER', 'STAFF', 'ADMIN')")
-    public Map<String, List<CategoryDTO>> getCategories() {
-        Map<String, List<CategoryDTO>> categoriesResponse = new HashMap<String, List<CategoryDTO>>();
-        categoriesResponse.put("categories", catalogService.getCategories());
-
-        return categoriesResponse;
-
-    }
-
-    @GetMapping(path = "/catalogs")
+    @GetMapping
     @PreAuthorize(value = "hasAnyRole('ADMIN', 'STAFF', 'CUSTOMER')")
     public Map<String, CatalogDTO> getCatalog() {
         System.out.println(catalogService.getCatalog());
@@ -61,7 +60,7 @@ public class CatalogController {
         return catalogResponse;
     }
 
-    @PutMapping(path = "/catalog/inventory")
+    @PutMapping(path = "/inventory")
     @PreAuthorize(value = "hasAnyRole('ADMIN', 'STAFF')")
     public Map<String, Boolean> updateInventory(@RequestBody CatalogInventoryUpdate updatedInventory) {
         Map<String, Boolean> updateInventoryResponse = new HashMap<>();
@@ -75,7 +74,7 @@ public class CatalogController {
         return updateInventoryResponse;
     }
 
-    @PostMapping(path = "/catalog/create-image")
+    @PostMapping(path = "/images")
     @PreAuthorize(value = "hasAnyRole('ADMIN', 'STAFF', 'CUSTOMER')")
     public ImageDTO createImage(@RequestParam("file") MultipartFile image, @RequestParam("objectId") String objectId) {
     	
@@ -90,12 +89,6 @@ public class CatalogController {
     			.name(localImage.getName())
     			.url(localImage.getUrl())
     			.build();
-    }	
-    //
-    // @GetMapping(path = "/catalog/variations/{id}")
-    // public LineItem getByVariationId(@PathVariable(name = "id") String id) {
-    // return catalogService.getCategoryItemByVariationId(id);
-    // }
-    //
+    }
     
 }
