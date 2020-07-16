598db29d517bcce4c898fa0594824b8e07f78c65
Andrew <36050911+sidorchukandrew@users.noreply.github.com>
2020-07-16 09:45:40 -0400

    Added endpoint for deleting your own account

--- src/main/java/upcafe/controller/UserController.java
+++ src/main/java/upcafe/controller/UserController.java
@@ -2,6 +2,8 @@ package upcafe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import upcafe.annotation.CurrentUser;
 import upcafe.dto.users.UserDTO;
@@ -10,33 +12,40 @@ import upcafe.security.model.UserPrincipal;
 import upcafe.service.UserService;
 
 @RestController
+@RequestMapping(path = "/api/v1/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping(path = "/users/me")
+    @GetMapping(path = "/me")
     @PreAuthorize("hasAnyRole('CUSTOMER', 'STAFF', 'ADMIN')")
     public UserDTO getCurrentlyAuthenticatedUser(@CurrentUser UserPrincipal userPrincipal) {
         return userService.getUserById(userPrincipal.getId());
     }
+
+    @DeleteMapping(path = "/me")
+    @PreAuthorize("hasAnyRole('CUSTOMER', 'STAFF', 'ADMIN')")
+    public boolean deleteCurrentlyAuthenticatedUser(@CurrentUser UserPrincipal userPrincipal) {
+        return userService.deleteUserById(userPrincipal.getId());
+    }
     
-    @GetMapping(path = "/api/v1/users")
+    @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
     public Iterable<User> getAllUsersForAdminView() {
     	return userService.getUserEntities();
     }
     
-    @PutMapping(path = "/api/v1/users")
+    @PutMapping
     @PreAuthorize("hasRole('ADMIN')")
     public User updateUser(@RequestBody User updatedUser) {
     	System.out.println(updatedUser);
     	return userService.updateUser(updatedUser);
     }
 
-    @DeleteMapping(path = "/api/v1/users/{id}")
+    @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public boolean deleteuser(@PathVariable(name = "id") int id) {
+    public boolean deleteUser(@PathVariable(name = "id") int id) {
         userService.deleteUserById(id);
         return true;
     }
